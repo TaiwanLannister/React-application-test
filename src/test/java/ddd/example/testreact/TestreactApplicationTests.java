44f14dfd52bf755d62feb8ca11a190a98c06d762
TaiwanLannister <dat1110000@gmail.com>
2024-06-08 10:58:27 +0700

    [TaiwanLannister] First commit, initialized project

---
+++ src/test/java/ddd/example/testreact/TestreactApplicationTests.java
@@ -0,0 +1,13 @@
+package ddd.example.testreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
